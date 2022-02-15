63ecd19fb01d452a9cc7a37aadabe25c1c030584
Novopashin Vladimir <vnovopashin@vtb.ru>
2022-02-15 18:36:51 +0300

    AC

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 a,b,c = [int(x) for x in input().split()]
 x,y,z = [int(x) for x in input().split()]
 
-#3 3 1
-#2 2 1
+ax = min(a,x)
+a -= ax
+x -= ax
 
-#5 6 5
-#6 5 6
+by = min (b,y)
+b -= by
+y -= by
 
-#5 6 0
-#6 5 0
-#There are no miracles in life
+cx = min (c,x)
+c -= cx
+x -= cx
 
-#6 5 0
-#5 6 0
-#There are no miracles in life
+cy = min (c,y)
+c -= cy
+y -= cy
+z -= a + b + c
 
-#3 6 2
-#6 5 1
-#There are no miracles in life
-
-#4 6 2
-#6 5 1
-#It is a kind of magic
-
-if (a>=x and b>=y and c>=z):
+if (x<=0 and y<= 0 and z<=0):
     print("It is a kind of magic")
 else:
     print("There are no miracles in life")
